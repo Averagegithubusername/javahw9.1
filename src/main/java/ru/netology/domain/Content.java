5347e61886e9b9f93ac8f7ce4d1a1abb937252ca
Averagegithubusername <s0mething_different@mail.ru>
2022-01-03 16:47:24 +0300

    rework

--- src/main/java/ru/netology/domain/Content.java
+++
@@ -1,8 +0,0 @@
-package ru.netology.domain;
-
-public class Content {
-    private String text;
-    private String[] mediaUrl;
-
-    //getter+setter
-}
--- src/main/java/ru/netology/domain/Header.java
+++
@@ -1,9 +0,0 @@
-package ru.netology.domain;
-
-public class Header {
-    private String avatarUrl;
-    private String name;
-    private String date;
-
-    //getter+setter
-}
